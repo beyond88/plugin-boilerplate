443e2637d90fc46ab5abbcdb9c1ff652bddd70ed
beyond88 <muhin.cse.diu@gmail.com>
2023-03-31 16:11:50 +0600

    added ajax class

--- includes/Ajax.php
+++ includes/Ajax.php
@@ -1,13 +1,6 @@
 <?php
 
-namespace Woo_Tripletex;
-use Woo_Tripletex\API\Handler\Order;
-use Woo_Tripletex\API\Handler\Customer;
-use Woo_Tripletex\Helpers;
-use Woo_Tripletex\Admin\Resources\OrderResource;
-use Woo_Tripletex\API\Handler\Voucher;
-
-use Woo_Tripletex\Admin\Cron\WooCommerceToTripletexOrder;
+namespace PluginBoilerplate;
 
 /**
  * Ajax handler class
